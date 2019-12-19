d415fddf914c5a9dec8ff0d7ed300120d709e4b8
VDuvarakesh <45750024+VDuvarakesh@users.noreply.github.com>
2019-12-19 13:59:38 +0530

    Add files via upload

---
+++ matrixdiagonalswap.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c[][]=new int[a][b];
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                c[i][j]=sc.nextInt();
+            }
+        }
+        int x=0;
+        int y=a-1;
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                if(i==j){
+                    int temp=c[i][j];
+                    c[i][j]=c[x][y];
+                    c[x][y]=temp;
+                    x++;
+                    y--;
+                }
+            }
+        }
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                System.out.print(c[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
