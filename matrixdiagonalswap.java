6711a6ae2d0b48453fcd80744285dbca8f16b708
VDuvarakesh <45750024+VDuvarakesh@users.noreply.github.com>
2019-12-20 16:08:38 +0530

    Update matrixdiagonalswap.java

--- matrixdiagonalswap.java
+++ matrixdiagonalswap.java
@@ -11,7 +11,7 @@ public class Main{
                 c[i][j]=sc.nextInt();
             }
         }
-        //int x=0;
+        int x=0;
         int y=a-1;
         for(int i=0;i<a;i++){
             for(int j=0;j<b;j++){
